5ce8b223b78927fddbcce2ce4f8be15f4578c2f3
Platform45 <platform45@platform45simac.capetown.45cloud.net>
2014-04-10 16:20:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RequirementTracker::Application.config.secret_key_base = '5c84f749b912efab4f4ce473f4c13bbf455231fe6d9a5748dcd646c9ad6cff43f2b6e8eacf7f9f24cf8dda87cae556c5c9fdfacaae114db5b6956fc4d325c18f'
